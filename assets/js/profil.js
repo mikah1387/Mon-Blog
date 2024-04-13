078ee514b879d93f88ce5945d0ad8c42fa89fabf
hakimo <achache.hakim@gmail.com>
2024-04-13 22:30:23 +0200

    add modal and style for page profile

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -2,7 +2,12 @@
 
 let links = document.querySelectorAll('.navprofil a')
 
+let btnmodifs = document.querySelectorAll('td .modifier')
+let btnsuprim = document.querySelectorAll('td .suprimer')
 
+let confirm = document.querySelector('#confirmLink');
+let paraph = document.querySelector('.modal-body p');
+let titre = document.querySelector('#staticBackdropLabel');
 links.forEach(link => {
     link.addEventListener('click', ()=>{
  document.querySelector('.navprofil a.active').classList.remove('active');
@@ -13,8 +18,40 @@ links.forEach(link => {
   document.querySelector('.main_profil .display').classList.remove('display')
   document.querySelector(attribut).classList.add('display');
     
-  console.log(document.querySelector(attribut));
+ 
 
  })
 
+});
+
+function replaceLink(link) {
+
+    confirm.href = link;
+}
+
+
+btnmodifs.forEach(btn => {
+    
+    btn.addEventListener('click', function(){
+
+        let link = this.getAttribute('href');
+        let slug = this.getAttribute('data-slug');
+        replaceLink(link);
+        
+        paraph.textContent ='Etes-vous sûr de vouloir modifier cet article';
+        titre.textContent = 'Modification de  l\'article ' + slug;
+    })
+});
+
+btnsuprim.forEach(btn => {
+    
+    btn.addEventListener('click', function(){
+
+        let link = this.getAttribute('href');
+        let slug = this.getAttribute('data-slug');
+        replaceLink(link);
+        
+        paraph.textContent ='Etes-vous sûr de vouloir suprimer cet article';
+        titre.textContent = 'Suppression de l\'article ' + slug;
+    })
 });
