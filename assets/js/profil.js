4d16ea4265394546617dbe0776b7107021b797d7
hakimo <achache.hakim@gmail.com>
2024-04-14 23:04:16 +0200

    work on page admin

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -1,6 +1,6 @@
 
 
-let links = document.querySelectorAll('.navprofil a')
+let links = document.querySelectorAll('.barnav a')
 
 let btnmodifs = document.querySelectorAll('td .modifier')
 let btnsuprim = document.querySelectorAll('td .suprimer')
@@ -8,14 +8,15 @@ let btnsuprim = document.querySelectorAll('td .suprimer')
 let confirm = document.querySelector('#confirmLink');
 let paraph = document.querySelector('.modal-body p');
 let titre = document.querySelector('#staticBackdropLabel');
+
 links.forEach(link => {
     link.addEventListener('click', ()=>{
- document.querySelector('.navprofil a.active').classList.remove('active');
+ document.querySelector('.barnav a.active').classList.remove('active');
   link.classList.add('active')
         
   let attribut = link.getAttribute("href");
 
-  document.querySelector('.main_profil .display').classList.remove('display')
+  document.querySelector('.main .display').classList.remove('display')
   document.querySelector(attribut).classList.add('display');
     
  
@@ -29,17 +30,35 @@ function replaceLink(link) {
     confirm.href = link;
 }
 
+function modifContext(mot, slug =''){
 
+    paraph.textContent ='Etes-vous sûr de vouloir le modifier ';
+    titre.textContent = 'Modification du '+ mot + slug ;
+}
+function suprimContext(mot, slug =''){
+
+    paraph.textContent ='Etes-vous sûr de vouloir le suprimer ';
+    titre.textContent = 'Suppression du '+ mot + slug ;
+}
 btnmodifs.forEach(btn => {
     
     btn.addEventListener('click', function(){
 
         let link = this.getAttribute('href');
         let slug = this.getAttribute('data-slug');
+    
+        if (link.includes('article')) {
+           
+            let mot = 'l\'article ';
+            modifContext(mot, slug);   
+        }else if (link.includes('comments')) {
+            let mot = 'commentaire';
+            modifContext(mot );
+            
+        }    
         replaceLink(link);
         
-        paraph.textContent ='Etes-vous sûr de vouloir modifier cet article';
-        titre.textContent = 'Modification de  l\'article ' + slug;
+
     })
 });
 
@@ -49,9 +68,18 @@ btnsuprim.forEach(btn => {
 
         let link = this.getAttribute('href');
         let slug = this.getAttribute('data-slug');
+        if (link.includes('article')) {
+           
+            let mot = 'l\'article ';
+            suprimContext(mot, slug);   
+        }else if (link.includes('comments')) {
+            let mot = 'commentaire';
+            suprimContext(mot);   
+
+            
+        }    
         replaceLink(link);
         
-        paraph.textContent ='Etes-vous sûr de vouloir suprimer cet article';
-        titre.textContent = 'Suppression de l\'article ' + slug;
+      
     })
 });
