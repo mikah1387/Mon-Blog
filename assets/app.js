bc362c76ec35604a6891be66834969c84e9c7008
hakimo <achache.hakim@gmail.com>
2024-05-05 17:45:32 +0200

    change test

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 
-// import './ckeditor/build/ckeditor.js';
-// import './js/ckeditor.js';
-// import './js/filters.js';
-// import './styles/scss/app.css'
-// import './js/navbar.js';
-// import './js/profil.js';
+import './ckeditor/build/ckeditor.js';
+import './js/ckeditor.js';
+import './js/filters.js';
+import './styles/scss/app.css'
+import './js/navbar.js';
+import './js/profil.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
 
