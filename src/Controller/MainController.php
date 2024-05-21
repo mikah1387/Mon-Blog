312099e0a5df38e587da409da47d11390c59577c
hakimo <achache.hakim@gmail.com>
2024-05-21 11:10:17 +0200

    change email contact

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -74,7 +74,7 @@ class MainController extends AbstractController
         ];
         $mail->send(
           $email,
-          'contact@sharearticle.ah-codeaddict.fr',
+          'contact_us@sharearticle.ah-codeaddict.fr',
           $subject,
           'contact',$context
           );
